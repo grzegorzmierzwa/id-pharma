4b5969e7b341d4e376df611ccf022b4562c1a556
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-11-30 21:24:08 +0100

    Add links

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// 'use strict';
+
+// document.getElementById('language').addEventListener('click', function () {
+//     // const links = document.querySelectorAll('a');
+//     // eslint-disable-next-line no-console
+//     console.log('links');
+// });
